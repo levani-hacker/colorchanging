3832c77c7c8a75b2336975541f053fefd7c7f229
levani-hacker <114279610+levani-hacker@users.noreply.github.com>
2023-08-24 15:22:12 +0400

    Add files via upload

---
+++ site.js
@@ -0,0 +1,9 @@
+// Get references to the elements
+const h1Element = document.getElementById("h1");
+const h4Element = document.getElementById("h4");
+const pElement = document.getElementById("p");
+
+// Change text colors using JavaScript
+h1Element.style.color = "red";
+h4Element.style.color = "green";
+pElement.style.color = "blue";
